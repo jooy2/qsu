76e1de8336bc3bb43112a3af8c22f23292ff1fd8
leejooy96 <leejooy96@gmail.com>
2021-03-27 12:22:14 +0900

    misc.takes error in nodejs unsupported environment

--- misc.js
+++ misc.js
@@ -1,16 +1,22 @@
-const { performance } = require('perf_hooks');
-
 const sleep = (delay = 0) => new Promise((resolve) => {
   setTimeout(resolve, delay);
 });
 
 const takes = (func, noDecimal = false) => {
-  const s = performance.now();
-  if (typeof func !== 'function') return 0;
-  func();
-  const e = performance.now() - s;
-  if (noDecimal) return Math.floor(e);
-  return e;
+  try {
+    // eslint-disable-next-line global-require
+    const { performance } = require('perf_hooks');
+    // do stuff
+    const s = performance.now();
+    if (typeof func !== 'function') return 0;
+    func();
+    const e = performance.now() - s;
+    if (noDecimal) return Math.floor(e);
+    return e;
+  } catch (e) {
+    // eslint-disable-next-line no-throw-literal
+    throw 'NodeJS environment is required to run this method.';
+  }
 };
 
 module.exports = {
