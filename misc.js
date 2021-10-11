63d8191889f07802a9b059c2669c0ab6c3a93b7d
leejooy96 <leejooy96@gmail.com>
2021-10-11 12:53:49 +0900

    remove unused function

--- misc.js
+++ misc.js
@@ -2,11 +2,6 @@ const sleep = (delay = 0) => new Promise((resolve) => {
   setTimeout(resolve, delay);
 });
 
-const nothing = (args) => {
-  if (args && typeof args === 'function') { args(); }
-};
-
 module.exports = {
   sleep,
-  nothing,
 };
