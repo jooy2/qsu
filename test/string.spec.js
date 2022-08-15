13c4b55907857492dc34e17ffb8df6a8e68da050
leejooy96 <leejooy96@gmail.com>
2022-08-15 12:06:10 +0900

    package: remove temporary test spec

--- test/string.spec.js
+++ test/string.spec.js
@@ -41,7 +41,6 @@ st`), 'test');
     assert.strictEqual(_.strNumberOf('abcdABCD', 'a'), 1);
     assert.strictEqual(_.strNumberOf('aaaaaa', 'a'), 6);
     assert.strictEqual(_.strNumberOf('hello', 'll'), 1);
-    assert.strictEqual(_.strNumberOf('hello', 'll'), 2); // TODO remove
     done();
   });
 
