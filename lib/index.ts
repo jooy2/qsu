cbfd9fe43a67072151af6e5b374b6e3d14d3a08d
leejooy96 <leejooy96@gmail.com>
2023-03-24 19:03:55 +0900

    typo: fix typo

--- lib/index.ts
+++ lib/index.ts
@@ -283,7 +283,7 @@ export default class Qsu {
 
 	static arrWithNumber(start: number, end: number): number[] {
 		if (start > end) {
-			throw new Error('end is greater than start.');
+			throw new Error('`end` is greater than start.');
 		}
 
 		return Array.from({ length: end - start + 1 }, (_, i) => i + start);
