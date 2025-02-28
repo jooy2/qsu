6cf21d47f3fc8ae4b444b25c6fc68a7221eccc31
leejooy96 <leejooy96@gmail.com>
2025-02-28 14:40:54 +0900

    test: fix test

--- lib/file/tailFile.ts
+++ lib/file/tailFile.ts
@@ -1,9 +1,10 @@
 import { readFile } from 'fs/promises';
+import { EOL } from 'os';
 
 export async function tailFile(filePath: string, length = 1): Promise<string | null> {
 	try {
 		const content = await readFile(filePath, 'utf-8');
-		const contentByLine = content.split(/[\r\n]+/g);
+		const contentByLine = content.split(EOL);
 		let result = '';
 
 		if (contentByLine[contentByLine.length - 1].length < 1) {
