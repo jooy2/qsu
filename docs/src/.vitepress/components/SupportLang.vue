8e2aa17c676c6e70e7fb5801dc37e62b540ba7a0
leejooy96 <leejooy96@gmail.com>
2025-09-25 14:00:28 +0900

    docs: add custom components

---
+++ docs/src/.vitepress/components/SupportLang.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+	dart: Boolean,
+	js: Boolean
+});
+</script>
+<template>
+	<Badge v-if="dart" type="info" text="Dart" />
+	<Badge v-if="js" type="tip" text="JavaScript" />
+</template>
--- docs/src/.vitepress/theme/index.js
+++ docs/src/.vitepress/theme/index.js
@@ -1,5 +1,11 @@
 import DefaultTheme from 'vitepress/theme';
 
 import './custom.css';
+import SupportLang from '../components/SupportLang.vue';
 
-export default DefaultTheme;
+export default {
+	extends: DefaultTheme,
+	enhanceApp({ app }) {
+		app.component('SupportLang', SupportLang);
+	}
+};
